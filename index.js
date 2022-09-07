575eb4ea4ed19e0bbaf1c8bd242941978dc8fffc
remy <pub@drijkoningen.net>
2022-09-07 21:59:37 +0200

    works

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-function round(number, decimals) {
+function round_number(number, decimals) {
     const multiplier = 10 ** decimals;
     return Math.round(number * multiplier) / multiplier;
 }
 
 class Tee {
+
+    constructor(holes) {
+        this._holes = holes;
+    }
+
+    get holes() {
+        return this._holes;
+    }
 
     set slope(slope) {
         if (this._slope)  throw "already initialized";
@@ -16,62 +24,71 @@ class Tee {
     }
 
     set rating9(rating9) {
-        if (this._rating)  throw "already initialized";
-        this._rating = 2 * rating9;
+        if (this._rating18)  throw "already initialized";
+        this._rating18 = 2 * rating9;
     }
 
     get rating9() {
-        return this._rating / 2.0;
+        return this._rating18 / 2.0;
     }
 
     set rating18(rating18) {
-        if (this._rating)  throw "already initialized";
-        this._rating = rating18;
+        if (this._rating18)  throw "immutable";
+        this._rating18 = rating18;
     }
 
     get rating18() {
-        return this._rating;
+        return this._rating18;
     }
 
     set par9(par9) {
-        if (this._par)  throw "already initialized";
-        this._par = 2 * par9;
+        if (this._par18)  throw "immutable";
+        this._par18 = 2 * par9;
     }
 
     get par9() {
-        return this._par / 2.0;
+        return this._par18 / 2.0;
     }
 
     set par18(par18) {
-        if (this._par)  throw "already initialized";
-        this._par = par18;
+        if (this._par18)  throw "immutable";
+        this._par18 = par18;
     }
 
-    get par18() {
+    set par(info) {
+        if (info.length != this.holes) throw "incorrect length";
+        this._par = info;
+    }
+
+    get par() {
         return this._par;
     }
 
-
-    course_handicap18(player) {
-        return round(player.index * this.slope / 113.0 + (this.rating18 - this.par18), 0);
+    set stroke_index(info) {
+        if (info.length != this.holes) throw "incorrect length";
+        this._stroke_index = info;
     }
 
-    course_handicap9(player) {
-        return round(this.course_handicap18(player) / 2.0, 0);
+    get stroke_index() {
+        return this._stroke_index;
+    }
+
+    get par18() {
+        return this._par18;
+    }
+
+    course_handicap(player) {
+        const course_handicap18 = player.index * this.slope / 113.0 + (this.rating18 - this.par18);
+        return this.holes == 18 ? round_number(course_handicap18, 0) : round_number(course_handicap18 / 2.0, 0);
     }
 
 }
 
-class ScoreCard {
+class Round {
 
-    constructor(player, tee, holes) {
+    constructor(player, tee) {
         this._player = player;
         this._tee = tee;
-        this._holes = holes;
-    }
-
-    get holes() {
-        return this._holes;
     }
 
     get player() {
@@ -83,28 +100,36 @@ class ScoreCard {
     }
 
     get course_handicap() {
-        return this._holes == 18 ? this.tee.course_handicap18(this.player) : this.tee.course_handicap9(this.player);
+        return this.tee.course_handicap(this.player);
+    }
+
+    _hole_multiplier() {
+        return 18 / this.tee.holes;
     }
 
     _score_differential(played_course_handicap18) {
-        const score_differential = round((played_course_handicap18 - (this.tee.rating18 - this.tee.par18)) * 113.0 / this.tee.slope, 1);
+        const score_differential = round_number((played_course_handicap18 - (this.tee.rating18 - this.tee.par18)) * 113.0 / this.tee.slope, 1);
         return {
-            course_handicap_played : this.holes == 18 ? played_course_handicap18 : played_course_handicap18 / 2.0,
+            course_handicap_played : played_course_handicap18 / this._hole_multiplier(),
             score_differential : score_differential
         }
     }
 
     stableford(score) {
-        const score18 = this.holes == 18 ? score : score + this.holes * 2 - 1;
-        const played_course_handicap18 = this.tee.course_handicap18(this.player) - (score18 - 36);
+        const score18 = this.tee.holes == 18 ? score : score + 17;
+        const played_course_handicap18 = this.course_handicap * this._hole_multiplier() - (score18 - 36);
         return this._score_differential(played_course_handicap18);
     }
 
-    gross(score) {
-        const score18 = this.holes == 18 ? score : score + this.tee.par9 + this.tee.course_handicap9(this.player) + 1;
-        console.log(score18);
+    gross_adjusted(score) {
+        const score18 = this.tee.holes == 18 ? score : score + this.tee.par9 + this.course_handicap + 1;
         const played_course_handicap18 = score18 - this.tee.par18;
         return this._score_differential(played_course_handicap18);
+    }
+
+    strokes(...info) {
+        if (info.length != this.tee.holes) throw "incorrect length";
+        this._strokes = info;
     }
 
 }
@@ -122,23 +147,52 @@ class Player {
         return this._index;
     }
 
-    plays9(tee) {
-        return new ScoreCard(this, tee, 9);
+    plays(tee) {
+        return new Round(this, tee);
     }
 
-    plays18(tee) {
-        return new ScoreCard(this, tee, 18);
-    }
 }
 
 
 
-american_blue_tee = new Tee();
-american_blue_tee.slope = 126;
-american_blue_tee.rating9 = 36;
-american_blue_tee.par18 = 70;
+const drohme_men_white = new Tee(9);
+drohme_men_white.slope = 98;
+drohme_men_white.rating18 = 61.2;
+drohme_men_white.par9 = 31;
 
-remy = new Player(14.2);
-const score_card = remy.plays9(american_blue_tee);
-console.log(score_card.gross(46));
-console.log(score_card.stableford(16));
+const drohme_men_blue = new Tee(9);
+drohme_men_blue.slope = 96;
+drohme_men_blue.rating18 = 60;
+drohme_men_blue.par9 = 31;
+
+const tournette_american9_men_blue = new Tee(9);
+tournette_american9_men_blue.slope = 123;
+tournette_american9_men_blue.rating18 = 69.2;
+tournette_american9_men_blue.par9 = 36;
+tournette_american9_men_blue.par = [4,4,4,3,4,5,3,5,4];
+tournette_american9_men_blue.stroke_index = [3,17,1,15,5,11,9,13,7]
+
+const tournette_american9_women_orange = new Tee(9);
+tournette_american9_women_orange.slope = 112;
+tournette_american9_women_orange.rating18 = 63.8;
+tournette_american9_women_orange.par9 = 36;
+
+const tournette_orival_men_blue = new Tee(9);
+tournette_orival_men_blue.slope = 55;
+tournette_orival_men_blue.rating9 = 23.4;
+tournette_orival_men_blue.par9 = 27;
+
+const tournette_orival_women_blue = new Tee(9);
+tournette_orival_women_blue.slope = 56;
+tournette_orival_women_blue.rating9 = 23.9;
+tournette_orival_women_blue.par9 = 27;
+
+
+const remy = new Player(36);
+const ying = new Player(50);
+
+const round = remy.plays(tournette_american9_men_blue);
+
+console.log(round.strokes(6, 10, 7, 3, 7, 6, 4, 6, 8));
+console.log(round.course_handicap);
+console.log(round.stableford(25));
